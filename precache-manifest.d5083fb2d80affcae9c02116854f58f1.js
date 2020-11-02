6d006c67bba8fe43bac752b0e29cdc44ea294640
Ronnie434 <56054439+Ronnie434@users.noreply.github.com>
2020-11-01 17:33:15 -0800

    Updates

---
+++ precache-manifest.d5083fb2d80affcae9c02116854f58f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526aea17ba95b436f36ce07d0d4fd956",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "d3be885629c8d2597b11",
+    "url": "/tic-tac-toe/static/css/main.48b54a10.chunk.css"
+  },
+  {
+    "revision": "a271450241b56c374b63",
+    "url": "/tic-tac-toe/static/js/2.002534bd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.002534bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3be885629c8d2597b11",
+    "url": "/tic-tac-toe/static/js/main.f3e9ec70.chunk.js"
+  },
+  {
+    "revision": "7f32745e7b69030faee7",
+    "url": "/tic-tac-toe/static/js/runtime-main.df28867d.js"
+  }
+]);
